690b1322671b0717015bfb115a9f1f97039f5e19
Mael Galite <mgalite@google.com>
2020-10-28 14:17:40 +0100

    Fixed provider zone and regions

--- tf/provider.tf
+++ tf/provider.tf
@@ -1,5 +1,5 @@
 provider "google" {
-  project     = var.project_id
-  region      = "eu-west1"
-  zone        = "eu-west1-b"
+  project = var.project_id
+  region  = "europe-west1"
+  zone    = "europe-west1-b"
 }
